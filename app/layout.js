5ebd589af4a4040d3c5443f9e8e0224c5f865959
sahilmahendrakar <sahilm020@gmail.com>
2023-12-17 01:29:34 -0500

    Barebones website scaffolding

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import Link from 'next/link'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +13,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <nav class="flex items-center justify-between flex-wrap bg-blue-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Book Buddy</span>
+      </div>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-white-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <Link href="/" class="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-white mr-4">
+            Home
+          </Link>
+          <Link href="/author" class="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-white mr-4">
+            Author
+          </Link>
+          <Link href="/user" class="block mt-4 lg:inline-block lg:mt-0 text-white-200 hover:text-white">
+            User
+          </Link>
+        </div>
+      </div>
+      </nav>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ app/user/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1 class="semi-bold p-6">Hello, this is the User page!</h1>
+  }
