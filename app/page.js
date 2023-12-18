851af9ff31f7b9886ba808d61ccdac51d15de816
Rachel Chung <rkc2134@columbia.edu>
2023-12-18 00:37:00 -0500

    link signup to sso link

--- app/page.js
+++ app/page.js
@@ -4,9 +4,11 @@ export default function Page() {
   return <>
     <h1 class="semi-bold p-6">Hello, Welcome to the Home page!</h1>
     <div class="flex flex-col items-center justify-center">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-        Sign in
-      </button>
+      <a href="http://localhost:8080">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Sign in
+        </button>
+      </a>
     </div>
   </>
 }
