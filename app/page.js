e6e54c5a6ad60522b7ebb59915b8ce3915cb74ea
Sanmati Choudhary <sanmatichoudhary@Sanmatis-MacBook-Pro.local>
2023-12-18 23:04:35 -0500

    changed link to gcloud

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
   return <>
     <h1 class="semi-bold p-6">Hello, Welcome to the Home page!</h1>
     <div class="flex flex-col items-center justify-center">
-      <a href="http://localhost:8080">
+      <a href="https://e6156-users-402619.ue.r.appspot.com">
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Sign in
         </button>
